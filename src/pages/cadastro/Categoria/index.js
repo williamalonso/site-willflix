8be9ca9f106ecf632211b7757aa9308761667935
“William <williamalonso@outlook.com>
2020-10-24 01:50:57 -0300

    aprendendo a usar o state no formulario de cadastro de categorias de videos

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+
+    const [categorias, setCategoria] = useState(['Teste']);
+    const valoresIniciais = {
+      nome: 'Nome Inicial',
+      descricao: 'Descrição Inicial',
+      cor: '#000',
+    }
+    const [values, setValues] = useState(valoresIniciais);
+
     return(
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: { values.nome } </h1>
 
-        <form>
+        <form onSubmit={function handleSubmit(infosDoEvento){
+          infosDoEvento.preventDefault();
+          setCategoria([
+            ...categorias,
+            values
+          ]);
+        }}>
+
+        <div>
             <label>
                 Nome da Categoria:
-                <input type="text" />
+                <input type="text" value={values.nome}
+                onChange = { function funcaoQueOerroPediu(infosDoEvento){
+                  setValues(infosDoEvento.target.value);
+                }} />
             </label>
+
+            <label>
+                Descrição:
+                <textarea type="text" value={values.descricao}
+                onChange = { function funcaoQueOerroPediu(infosDoEvento){
+                  setValues(infosDoEvento.target.value);
+                }} />
+            </label>
+
+            <label>
+                Cor:
+                <input type="color" value={values.cor}
+                onChange = { function funcaoQueOerroPediu(infosDoEvento){
+                  setValues(infosDoEvento.target.value);
+                }} />
+            </label>
+        </div>
+
+            
             <button>Cadastrar</button>
         </form>
+
+        <ul>
+          {categorias.map( (categoria, indice) => {
+            return (
+              <li key={`${categoria}${indice}`} >
+                {categoria}
+              </li>
+            )
+          } )}
+        </ul>
 
         <Link to="/">
           Ir para Home
