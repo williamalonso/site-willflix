2eba26f4d3694f3f0fa8b1bc3d0808e0ee67c3d9
William Afonso Lemos Alonso <william.alonso@PRF.SEDE>
2021-06-21 22:42:57 -0300

    comentário

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import Button from '../Button';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // para utilizar o SPA
+
 //import ButtonLink from './components/ButtonLink';
+
 import './Menu.css';
 
 function Menu(){
     return(
-        <nav className="Menu">
-            <Link to="/">
+        <nav className="Menu"> 
+            <Link to="/"> {/* substitui a tag <a href="/">...</a> para utilizar SPA */}
                 <img className="Logo" src={Logo} alt="Willflix logo" />
             </Link>
             <Button as={Link} to="/cadastro/video" className="ButtonLink">
