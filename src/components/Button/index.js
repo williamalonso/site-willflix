c350bf6d6990503192818f1123ee82d1108a78a8
William Afonso Lemos Alonso <william.alonso@PRF.SEDE>
2021-06-25 21:41:55 -0300

    comentario

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom'; // para utilizar o SPA
 
 const Button = styled(Link)`
     color: var(--white);
